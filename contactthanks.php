b7f5672d24dc4f59e8dccce86d2de992a854747a
Alec <50312581+albjorns@users.noreply.github.com>
2019-07-12 09:36:20 -0700

    Initial Upload

---
+++ contactthanks.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+	<title>Message Submitted</title>
+  <link rel="shortcut icon" href="images/paw.ico">
+  <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/styles.css" />
+</head>
+
+<body>
+
+	<div id="page-wrap">
+
+		<h1>Your message has been sent!</h1><br />
+
+		<p><a href="/index.html">Back to Contact Form</a></p>
+
+	</div>
+
+</body>
+
+</html>
